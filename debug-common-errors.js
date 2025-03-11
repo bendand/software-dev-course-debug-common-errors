69b30ca97fb436d5a92e6af7b6805fa1d8d2db60
Ben <bendanderson13@gmail.com>
2025-03-10 21:52:39 -0500

    a commit

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -21,26 +21,26 @@ Think about which debugging methods you found most useful and how you might appl
 // Programs and Solutions
 
 // Program A
-// Description:
+// Description: 
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
-
-
+// This program contained two Syntax Errors- one missing closing " and one missing closing ). I added these and now it runs fine.
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
+// This above is a runtime error- we are multiplying a string spelling the number 8 by another number, which confuses the program
+// and we get an erroneous output of 'NaN'. I changed 'eight' to 8 and now everything works fine.
 
 
 // Program C (Logic Error)
@@ -51,12 +51,17 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+// There is a logic error in the progam- the conditions under which we return true vs false are swtiched. 
+// The correct behavior of this code after the Logic Error is rectified is that false is returned if there
+// is any number less that the number given as the input that the input number can be divided by that would
+// produce 0 as the remainder. This indicates that there are other numbers besides 1 and the given number itself 
+// that the given number is divisible by, and therefore that the number is not prime.
